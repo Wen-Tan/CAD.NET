0b6e3d6656322fef20fa0e17983f594281871b10
Wen-Tan <duwentan26@qq.com>
2022-07-17 12:10:18 +0800

    弹窗

---
+++ Demo1/Demo1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//新添加的头文件
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+
+namespace Demo1
+{
+    public class Class1
+    {
+        //加一个命令
+        [CommandMethod("demo1")]
+        //写函数
+        public void demo1()
+        {
+            //声明命令行对象
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("我是无敌小可爱");
+        }
+    }
+}
